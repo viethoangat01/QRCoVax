628fb731a60177397d1aebda94de242598e0d045
Việt Hoàng <52716298+viethoangat01@users.noreply.github.com>
2021-12-18 01:15:25 +0700

    Update login with Firebase authentication
    Login with OTP, resend OTP
    
    Signed-off-by: Viet Hoang <viethoangat01@gmail.com>

--- app/src/main/java/com/example/qrcovax/view/LoginActivity.java
+++ app/src/main/java/com/example/qrcovax/view/LoginActivity.java
@@ -5,15 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.qrcovax.R;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText edtPhoneNumber;
     Button btnLogin;
+    EditText edtPhoneNumber;
+    ProgressBar pbSend;
     private static final String PHONENUMBER = "phonenumber";
+    private static final String VERIFICATION_ID = "verificationId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +36,41 @@ public class LoginActivity extends AppCompatActivity {
                 if (edtPhoneNumber.getText().toString().trim().isEmpty()) {
                     edtPhoneNumber.setError(getString(R.string.login_errorinput));
                 } else {
-                    Intent intent = new Intent(getApplicationContext(), VerifyOtpActivity.class);
-                    intent.putExtra(PHONENUMBER, edtPhoneNumber.getText().toString());
-                    startActivity(intent);
+                    pbSend.setVisibility(View.VISIBLE);
+                    btnLogin.setVisibility(View.INVISIBLE);
+
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            "+84" + edtPhoneNumber.getText().toString(),
+                            60,
+                            TimeUnit.SECONDS,
+                            LoginActivity.this,
+                            new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                                @Override
+                                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                    pbSend.setVisibility(View.GONE);
+                                    btnLogin.setVisibility(View.VISIBLE);
+                                }
+
+                                @Override
+                                public void onVerificationFailed(@NonNull FirebaseException e) {
+                                    pbSend.setVisibility(View.GONE);
+                                    btnLogin.setVisibility(View.VISIBLE);
+                                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+
+                                @Override
+                                public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                    super.onCodeSent(verificationId, forceResendingToken);
+                                    pbSend.setVisibility(View.GONE);
+                                    btnLogin.setVisibility(View.VISIBLE);
+                                    Intent intent = new Intent(getApplicationContext(), VerifyOtpActivity.class);
+                                    intent.putExtra(PHONENUMBER, edtPhoneNumber.getText().toString());
+                                    intent.putExtra(VERIFICATION_ID, verificationId);
+                                    startActivity(intent);
+                                }
+                            }
+                    );
                 }
             }
         });
@@ -37,5 +79,6 @@ public class LoginActivity extends AppCompatActivity {
     private void initView() {
         btnLogin = (Button) findViewById(R.id.button_login);
         edtPhoneNumber = (EditText) findViewById(R.id.edittext_login_phonenumber);
+        pbSend = (ProgressBar) findViewById(R.id.progressbar_login);
     }
 }
--- app/src/main/java/com/example/qrcovax/view/VerifyOtpActivity.java
+++ app/src/main/java/com/example/qrcovax/view/VerifyOtpActivity.java
@@ -1,62 +1,141 @@
 package com.example.qrcovax.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.qrcovax.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class VerifyOtpActivity extends AppCompatActivity {
+    private Button btnVerify;
     private EditText edtCode1, edtCode2, edtCode3, edtCode4, edtCode5, edtCode6;
-    private TextView tvPhoneNumber;
+    private ProgressBar pbLoad;
+    private TextView tvPhoneNumber, tvResend;
     private static final String PHONENUMBER = "phonenumber";
+    private static final String VERIFICATION_ID = "verificationId";
+    private static final String FORMAT_PHONE = "(+84) %s";
+
+    private String verificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_otp);
         initView();
+        verificationId = getIntent().getStringExtra(VERIFICATION_ID);
         tvPhoneNumber.setText(String.format(
-                "(+84) %s", getIntent().getStringExtra(PHONENUMBER)
+                FORMAT_PHONE, getIntent().getStringExtra(PHONENUMBER)
         ));
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtCode1.getText().toString().trim().isEmpty()
+                        || edtCode2.getText().toString().trim().isEmpty()
+                        || edtCode3.getText().toString().trim().isEmpty()
+                        || edtCode4.getText().toString().trim().isEmpty()
+                        || edtCode5.getText().toString().trim().isEmpty()
+                        || edtCode6.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(VerifyOtpActivity.this, R.string.verifyotp_entervalidcode, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String code = edtCode1.getText().toString() +
+                        edtCode2.getText().toString() +
+                        edtCode3.getText().toString() +
+                        edtCode4.getText().toString() +
+                        edtCode5.getText().toString() +
+                        edtCode6.getText().toString();
+                String verificationId = getIntent().getStringExtra(VERIFICATION_ID);
+                if (verificationId != null) {
+                    pbLoad.setVisibility(View.VISIBLE);
+                    btnVerify.setVisibility(View.INVISIBLE);
+                    PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(
+                            verificationId, code
+                    );
+                    FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    pbLoad.setVisibility(View.GONE);
+                                    btnVerify.setVisibility(View.VISIBLE);
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(VerifyOtpActivity.this, R.string.verifyotp_codeinvalid, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+        tvResend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        "+84" + getIntent().getStringExtra(PHONENUMBER),
+                        60,
+                        TimeUnit.SECONDS,
+                        VerifyOtpActivity.this,
+                        new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String newVerificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(newVerificationId, forceResendingToken);
+                                verificationId = newVerificationId;
+                                Toast.makeText(VerifyOtpActivity.this, "OTP Sent", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+            }
+        });
         setupOTPInputs();
     }
 
     private void initView() {
+        btnVerify = (Button) findViewById(R.id.button_verifyotp_verify);
         edtCode1 = (EditText) findViewById(R.id.edittext_verifyotp_1);
         edtCode2 = (EditText) findViewById(R.id.edittext_verifyotp_2);
         edtCode3 = (EditText) findViewById(R.id.edittext_verifyotp_3);
         edtCode4 = (EditText) findViewById(R.id.edittext_verifyotp_4);
         edtCode5 = (EditText) findViewById(R.id.edittext_verifyotp_5);
         edtCode6 = (EditText) findViewById(R.id.edittext_verifyotp_6);
+        pbLoad = (ProgressBar) findViewById(R.id.progressbar_verifyotp);
         tvPhoneNumber = (TextView) findViewById(R.id.textview_verifyotp_phonenumber);
+        tvResend = (TextView) findViewById(R.id.textview_verifyotp_resend);
     }
 
     private void setupOTPInputs() {
         edtCode1.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (!charSequence.toString().trim().isEmpty()) {
-                    edtCode1.requestFocus();
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-        edtCode2.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -75,7 +154,7 @@ public class VerifyOtpActivity extends AppCompatActivity {
 
             }
         });
-        edtCode3.addTextChangedListener(new TextWatcher() {
+        edtCode2.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -94,7 +173,7 @@ public class VerifyOtpActivity extends AppCompatActivity {
 
             }
         });
-        edtCode4.addTextChangedListener(new TextWatcher() {
+        edtCode3.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -113,7 +192,7 @@ public class VerifyOtpActivity extends AppCompatActivity {
 
             }
         });
-        edtCode5.addTextChangedListener(new TextWatcher() {
+        edtCode4.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -132,7 +211,7 @@ public class VerifyOtpActivity extends AppCompatActivity {
 
             }
         });
-        edtCode6.addTextChangedListener(new TextWatcher() {
+        edtCode5.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 

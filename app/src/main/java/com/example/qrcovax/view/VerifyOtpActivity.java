b024f3e5b58d7d579cb7ad7e9a4691dd746ee710
Viet Hoang <viethoangat01@gmail.com>
2021-12-18 13:37:19 +0700

    Register profile
    Register profile after authenticating OTP
    
    Signed-off-by: Viet Hoang <viethoangat01@gmail.com>

--- app/src/main/java/com/example/qrcovax/view/VerifyOtpActivity.java
+++ app/src/main/java/com/example/qrcovax/view/VerifyOtpActivity.java
@@ -79,6 +79,7 @@ public class VerifyOtpActivity extends AppCompatActivity {
                                     if (task.isSuccessful()) {
                                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
                                         startActivity(intent);
                                     } else {
                                         Toast.makeText(VerifyOtpActivity.this, R.string.verifyotp_codeinvalid, Toast.LENGTH_SHORT).show();
